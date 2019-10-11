872971be515d79fbbc494df5b0bb25e4d3453d24
JacksonMcLane <jackson.mclane20@gmail.com>
2019-10-11 14:08:50 -0700

    Added question class Made json reader

---
+++ app/src/main/java/com/example/quizjava/Question.java
@@ -0,0 +1,11 @@
+package com.example.quizjava;
+
+public class Question {
+    private String question;
+    private String answer;
+
+    @Override
+    public String toString() {
+        return question;
+    }
+}
