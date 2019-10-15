235df7c86dbdedfe5e4b7932bedd570eb2843a27
JacksonMcLane <jackson.mclane20@gmail.com>
2019-10-15 13:51:43 -0700

    Small framework methods made

--- app/src/main/java/com/example/quizjava/Question.java
+++ app/src/main/java/com/example/quizjava/Question.java
@@ -2,10 +2,15 @@ package com.example.quizjava;
 
 public class Question {
     private String question;
-    private String answer;
+    private boolean answer;
 
     @Override
     public String toString() {
         return question;
     }
+
+    public boolean getAnswer(){
+        return answer;
+    }
+
 }
---
+++ app/src/main/java/com/example/quizjava/Quiz.java
@@ -0,0 +1,53 @@
+package com.example.quizjava;
+
+import java.util.List;
+
+public class Quiz {
+    int currentQuestion;
+    List QuestionList;
+    Question question;
+    int score;
+
+    public int getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(int currentQuestion) {
+        this.currentQuestion = currentQuestion;
+    }
+
+    public List getQuestionList() {
+        return QuestionList;
+    }
+
+    public void setQuestionList(List questionList) {
+        QuestionList = questionList;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public boolean checkAnswer(boolean answer){
+//        if(answer == QuestionList.get(currentQuestion.getAnswer())){
+//            return true;
+//        }
+        return false;
+    }
+
+    public boolean isQuestionsRemaining(){
+        if(currentQuestion+1 < QuestionList.size()-1) {
+            return true;
+        }
+        else{ return false; }
+    }
+
+    public void nextQuestion(){
+        currentQuestion++;
+    }
+
+}
