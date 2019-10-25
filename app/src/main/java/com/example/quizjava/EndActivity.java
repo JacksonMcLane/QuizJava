32ade27a67f04364129735c6e1d60e945e248e7e
JacksonMcLane <jackson.mclane20@gmail.com>
2019-10-25 14:01:23 -0700

    Finished Quiz

--- app/src/main/java/com/example/quizjava/EndActivity.java
+++ app/src/main/java/com/example/quizjava/EndActivity.java
@@ -2,27 +2,42 @@ package com.example.quizjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class EndActivity extends AppCompatActivity {
 
     private TextView textViewScore;
+    private Button buttonRestart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end);
         wireWidgets();
+        setListeners();
         Intent lastIntent = getIntent();
-        int score = lastIntent.getIntExtra(MainActivity.EXTRA_SCORE, 0);
-        Log.d("End Activity", "onCreate: " + score);
+        int score = lastIntent.getIntExtra(MainActivity.EXTRA_SCORE, -1);
         textViewScore.setText(getString(R.string.score_textView) + score);
+    }
+
+    private void setListeners() {
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+
+            }
+        });
     }
 
     private void wireWidgets() {
         textViewScore = findViewById(R.id.textView_end_score);
+        buttonRestart = findViewById(R.id.button_end_restart);
     }
 }
--- app/src/main/java/com/example/quizjava/Quiz.java
+++ app/src/main/java/com/example/quizjava/Quiz.java
@@ -10,21 +10,20 @@ public class Quiz {
     public Quiz(List<Question> QuestionList) {
         this.QuestionList = QuestionList;
         currentQuestion = 0;
-    }
-
-
-    public int getScore() {
-        return score;
+        score = 0;
     }
 
     public void setScore(int score) {
         this.score = score;
     }
 
+    public int getScore() {
+        return score;
+    }
+
     public boolean checkAnswer(boolean answer) {
         if(answer == QuestionList.get(currentQuestion).getAnswer()) {
             score += 100;
-
             return true;
         }
         return false;
@@ -38,11 +37,19 @@ public class Quiz {
         if (currentQuestion + 1 <= QuestionList.size() - 1) {
             return true;
         }
-        else{ return false; }
+        return false;
     }
 
     public void nextQuestion(){
         currentQuestion++;
+    }
+
+    public String getCurrentQuestion() {
+        return currentQuestion+1 + ". ";
+    }
+
+    public void setCurrentQuestion(int currentQuestion) {
+        this.currentQuestion = currentQuestion;
     }
 
 }
